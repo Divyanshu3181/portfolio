67a89fafeb6267a549c689891c81619993707712
Divyanshu Sharma <sharmadivyanshu9752@gmail.com>
2025-02-24 11:17:34 +0530

    edit resume link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -99,7 +99,7 @@ export default function Hero() {
               Contact Me
             </motion.a>
             <motion.a
-              href="https://drive.google.com/file/d/18NWfLJTgdcmlOZh_NLF20zEZWlwvQFvL/view?usp=sharing"
+              href="https://drive.google.com/file/d/11rbuu3_D74xoNP_MtlyZGxqQ2p_J5ynn/view?usp=sharing"
               className={`px-8 py-3 rounded-full text-white ${gradientBg} ${gradientHover} transition-all duration-300 shadow-lg shadow-purple-500/25`}
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
